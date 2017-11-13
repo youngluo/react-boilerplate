2e0411ae8a61681f9e6e89ae5dcecb0ac694f522
young <ly532265997@gmail.com>
2017-11-13 10:44:55 +0800

    调整目录结构

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import * as reducers from './reducer';
 import thunk from 'redux-thunk';
 
 //创建一个 Redux store 来存放应用中所有的 state，应用中有且仅有一个 store。
-let store = createStore(
+const store = createStore(
     combineReducers(reducers),
     applyMiddleware(thunk)
 );
